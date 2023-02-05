094d7b540b92537b3e6836d429cfe95472661ec5
Stanislau <xmix25@gmail.com>
2023-02-05 20:42:13 +0300

    done brackets checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let brackets = [];
+  for (let l of str) {
+    for (let b of bracketsConfig) {
+      if (l == b[0] && l == b[1]) {
+        brackets[brackets.length - 1] == l ? brackets.pop() : brackets.push(l);
+      } else if (l == b[0]) {
+        brackets.push(l);
+      } else if (l == b[1]) {
+        if (brackets[brackets.length - 1] == b[0]) {
+          brackets.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return !brackets.length;
+};
